5a975f037a72d1f4bde3479b8c2447b702b9c190
Mr-afk-cell <kboxser2026@kehillahstudent.org>
2024-11-16 14:30:29 -0800

    a07

--- ex1.py
+++ ex1.py
@@ -1,1 +1,9 @@
 # Use turtle graphics to make an infinite spiral
+import turtle
+turtle.speed
+i = 0 # i is indexing variable
+num = 30 # num is the line segment
+while i < num:
+    turtle.forward (20*i) #makes progressively large line segments 
+    turtle.right(90)
+    i = i + 1
